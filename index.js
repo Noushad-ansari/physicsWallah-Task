7a1e7d9727c7facd9eb0bd8c03a525fa043c4102
Noushad-ansari <nrawebdesigner@gmail.com>
2022-10-19 21:52:02 +0530

    modal added

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var extArr = [];
 let tableContainer = document.getElementById("table_Container");
-// let infoBtn = document.getElementById("infoBtn")
-
 
 let filesArray = []
 
@@ -10,20 +8,18 @@ let filesArray = []
 function fileInfo(event) {
 
     let files = event.files;
-    // console.log(files)
-
+    
     filesArray = Array.from(files).map(file => {
         let fileName = file.name
         let fileSize = file.size
         let fileExt = getFileExtention(file)
-        return { fileName, fileSize, fileExt }
+        let lastModifiedDate = file.lastModifiedDate
+        console.log(lastModifiedDate)
+        return { fileName, fileSize, fileExt,lastModifiedDate }
     })
 
     let sortedFile = fileSortByExtention(filesArray)
     showTableUi(sortedFile)
-    // console.log(sortedFile)
-    // showInfoOnMouseOver(sortedFile)
-
 
 }
 
@@ -47,7 +43,7 @@ function getFileExtention(file) {
 //9****************************SHow Table UI***************************
 function showTableUi(files) {
 
-   tableContainer.innerHTML = ' '
+    tableContainer.innerHTML = ' '
     let table = document.createElement("table")
     table.setAttribute("border", "border:2;");
     table.setAttribute("id", "table");
@@ -60,24 +56,15 @@ function showTableUi(files) {
                  <th>File Info</th>
             </tr>
          </thead>`
-    files.map((file, ) => {
+
+    files.map((file,) => {
+
         // remove extention
         const lastDot = file.fileName.lastIndexOf(".");
         const fileName = file.fileName.substring(0, lastDot)
 
 
-        let fileSize = file.fileSize
-        if (0 > fileSize || fileSize < 1024) {
-            fileSize = fileSize + "Bytes"
-        } else if (1024 <= fileSize && fileSize < 1048576) {
-            fileSize = (fileSize / 1024).toFixed(2) + "KB"
-        } else if (1048576 <= fileSize && fileSize < 1073741824) {
-            fileSize = (fileSize / 1048576).toFixed(2) + "MB"
-        } else if (1073741824 <= fileSize && fileSize < 1099511627776) {
-            fileSize = (fileSize / 1073741824).toFixed(2) + "GB"
-        } else if (1099511627776 < fileSize) {
-            fileSize = (fileSize / 1099511627776).toFixed(2) + "TB"
-        }
+        const fileSize = getFileSize(file)
 
         // Create TR & TD Element
         let tr = document.createElement("tr")
@@ -86,8 +73,8 @@ function showTableUi(files) {
         let td2 = document.createElement("td")
         td2.innerHTML = fileSize
         let td3 = document.createElement("td")
-        td3.innerHTML = `<button id="infoBtn" class = "btn btn-info m-2" onClick= showInfoOnMouseOver(${JSON.stringify(files).split('"').join("&quot;")})  >File Info</button>`
-        tr.appendChild(td1)
+        td3.innerHTML = `<button  data-toggle="modal" data-target="#exampleModalCenter" id="infoBtn" class = "btn btn-info m-2" onmouseover= showInfoOnMouseOver(${JSON.stringify(file).split('"').join("&quot;")})  >File Info</button>`
+        tr.appendChild(td1, td2, td3)
         tr.appendChild(td2)
         tr.appendChild(td3)
 
@@ -95,49 +82,101 @@ function showTableUi(files) {
         table.appendChild(tr)
     });
     table_Container.appendChild(table)
-
-
 }
 
+function getFileSize(file) {
+
+
+    let fileSize = file.fileSize
+    if (0 > fileSize || fileSize < 1024) {
+        fileSize = fileSize + "Bytes"
+    } else if (1024 <= fileSize && fileSize < 1048576) {
+        fileSize = (fileSize / 1024).toFixed(2) + "KB"
+    } else if (1048576 <= fileSize && fileSize < 1073741824) {
+        fileSize = (fileSize / 1048576).toFixed(2) + "MB"
+    } else if (1073741824 <= fileSize && fileSize < 1099511627776) {
+        fileSize = (fileSize / 1073741824).toFixed(2) + "GB"
+    } else if (1099511627776 < fileSize) {
+        fileSize = (fileSize / 1099511627776).toFixed(2) + "TB"
+    }
+
+    return fileSize
+}
 
 //******************* */ showInformation on mouse Hover************************
-function showInfoOnMouseOver(files) {
-    let fileName, fileSize, fileExtention
-    files.map(file => {
-        const lastDot = file.fileName.lastIndexOf(".");
-        fileName = file.fileName.substring(0, lastDot)
-        fileSize = file.fileSize
-        fileExtention = file.fileExt
-    })
+function showInfoOnMouseOver(file) {
+    let fileName, fileSize, fileExtention,lastModifiedDate
+    const lastDot = file.fileName.lastIndexOf(".");
+    fileName = file.fileName.substring(0, lastDot)
+    fileSize = getFileSize(file)
+    fileExtention = file.fileExt
+    lastModifiedDate = file.lastModifiedDate
 
-    alert(`FileName : ${fileName} , FileSize: ${fileSize} , extention: ${fileExtention}`)
+    // alert(`FileName : ${fileName} , FileSize: ${fileSize} , extention: ${fileExtention}`)
 
-    // <!-- Button trigger modal -->
-    //   let div = document.createElement("div")
-    //   div.innerHTML = `
+    let div = document.createElement("div")
+    div.innerHTML = `<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title text-info" id="exampleModalLongTitle">File Info</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+          
 
-    //   <!-- Modal -->
-    //   <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    //   <div class="modal-dialog modal-dialog-centered" role="document">
-    //     <div class="modal-content">
-    //       <div class="modal-header">
-    //         <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-    //         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-    //           <span aria-hidden="true">&times;</span>
-    //         </button>
-    //       </div>
-    //       <div class="modal-body">
-    //         ...
-    //       </div>
-    //       <div class="modal-footer">
-    //         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-    //         <button type="button" class="btn btn-primary">Save changes</button>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   </div>`
+          <!-- //file info container -->
+          <div class="main-content">
+            <div class="header bg-gradient-primary pb-8 pt-2 pt-md-8">
+              <div class="container-fluid">
+                <div class="header-body">
+                  <div class="row">
+                    <div class="col-xl-3 col-lg-6">
+                      <div class="card card-stats mb-4 mb-xl-0">
+                        <div class="card-body">
+                          <div class="row">
+                            <div class="col">
+                              <h5 class="card-title text-uppercase text-primary mb-0">File Name</h5>
+                           
+                              <span class="h2 font-weight-normal mb-0">${fileName}</span>
+                            </div>
+                            <div class="col-auto">
+                              <h5 class="card-title text-uppercase text-success bmb-0">Extention</h5>
+                              <span class="h2 font-weight-normal mb-0">.${fileExtention}</span>
+    
+                              <!-- <div class="icon icon-shape bg-info badge-info text-white rounded shadow">
+                                <i class="fas fa-chart-bar p-2"> Extention &nbsp; .${fileExtention}</i>
+                              </div> -->
+                            </div>
+                          </div>
+                          <p class="mt-3 mb-0 text-muted text-sm">
+                            <a href="../../../../../../../ALL CODING NOTES/C-language.pdf" class="text-danger mr-2" download><i class="fa fa-arrow-up"></i> File Size - ${fileSize}</a>
+                           
+                          </p>
+                          <span class="text-nowrap text-muted">Last Modified: ${lastModifiedDate}</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+    <!-- ******* -->
+    
+            
+    
 
-    //   document.body.appendChild(div)
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+      </div>`
+
+    document.body.appendChild(div)
 
 }
 
